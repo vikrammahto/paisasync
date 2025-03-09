d3d6c5d2e6e126bdc1e2cd5ea4e6bca9f7cbc293
vikrammahto <vikramkr.in@gmail.com>
2025-03-10 02:19:04 +0530

    Add coming soon content

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,42 @@
 export default function Home() {
   return (
-    <main className="flex h-dvh items-center justify-center">
-      <h1 className="text-xl">Hello world!</h1>
+    <main className="flex min-h-dvh items-center justify-center">
+      <div className="mx-auto max-w-2xl text-center sm:max-w-xl md:max-w-2xl">
+        <h1>
+          We’re building <span className="font-semibold">PaisaSync</span>
+        </h1>
+        <p>
+          A smart way to manage your money, expenses, budgets, debt, and savings
+          – all in one place.
+        </p>
+        <h2 className="mt-6">For updates</h2>
+        <div className="flex justify-center gap-4">
+          <a
+            href="https://x.com/here_vikram"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="cursor-pointer"
+          >
+            X (Twitter)
+          </a>
+          <a
+            href="https://www.linkedin.com/in/vikrammahto/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="cursor-pointer"
+          >
+            LinkedIn
+          </a>
+          <a
+            href=" https://github.com/vikrammahto"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="cursor-pointer"
+          >
+            GitHub
+          </a>
+        </div>
+      </div>
     </main>
   );
 }
